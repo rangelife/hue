54777746423636011e203a5920d1fcf76600442a
Rob Lundie Hill <rob@lundiehill.com>
2021-08-20 07:56:59 +0100

    add readme. tweak lights.js to test single room(group) toggle

---
+++ README.md
@@ -0,0 +1,3 @@
+do experiments on lights.js
+
+then move the logic into backend.js which is the server responding to REST calls
--- lights.js
+++ lights.js
@@ -11,6 +11,7 @@ let action;
 switch(process.argv[2]) {
 	case 'on': action=true; break;
 	case 'off': action=false; break;
+        case 'toggle': action='toggle'; break;
 }
 let client;
 
@@ -116,31 +117,103 @@ promise.then( function (ip) {
 
 // })
 
-.then( function (x) {
-	return client.rules.getAll()
-  .then(rules => {
-    for (let rule of rules) {
-      console.log(`Rule [${rule.id}]: ${rule.name}`);
-      console.log(`  Created:         ${rule.created}`);
-      console.log(`  Last Triggered:  ${rule.lastTriggered}`);
-      console.log(`  Times Triggered: ${rule.timesTriggered}`);
-      console.log(`  Owner:           ${rule.owner}`);
-      console.log(`  Status:          ${rule.status}`);
+//.then( function (x) {
+//	return client.rules.getAll()
+//  .then(rules => {
+//    for (let rule of rules) {
+//      console.log(`Rule [${rule.id}]: ${rule.name}`);
+//      console.log(`  Created:         ${rule.created}`);
+//      console.log(`  Last Triggered:  ${rule.lastTriggered}`);
+//      console.log(`  Times Triggered: ${rule.timesTriggered}`);
+//      console.log(`  Owner:           ${rule.owner}`);
+//      console.log(`  Status:          ${rule.status}`);
+//
+//      console.log(`  Conditions:`);
+//      for (let condition of rule.conditions) {
+//        console.log(`    Address:  ${condition.address}`);
+//        console.log(`    Operator: ${condition.operator}`);
+//        console.log(`    Value:    ${condition.value}`);
+//        console.log();
+//      }
+//
+//      console.log(`  Actions:`);
+//      for (let action of rule.actions) {
+//        console.log(`    Address: ${action.address}`);
+//        console.log(`    Method:  ${action.method}`);
+//        console.log(`    Body:    ${JSON.stringify(action.body)}`);
+//        console.log();
+//      }
+//
+//      console.log();
+//    }
+//  });
+//})
 
-      console.log(`  Conditions:`);
-      for (let condition of rule.conditions) {
-        console.log(`    Address:  ${condition.address}`);
-        console.log(`    Operator: ${condition.operator}`);
-        console.log(`    Value:    ${condition.value}`);
-        console.log();
+
+// all lights i think
+//.then( function (x) {
+//	return client.groups.getById(0)
+//  .then(group => {
+//    console.log('Special group 0');
+//    console.log(JSON.stringify(group,0,4));
+//    console.log('  Light Ids:', group.lightIds.join(', '));
+//
+// 	if (action !== undefined) {
+//	    group.on = action;
+//	    console.log(JSON.stringify(group,0,4));
+//
+//	    return client.groups.save(group).then( function (x) {
+//			console.log("saved");
+//		});
+//	}
+//  });
+//})
+
+.then(function(x) {
+  return client.groups.getAll()
+  .then(groups => {
+    for (let group of groups) {
+      if (group.name !== "Main bedroom") {
+        continue;
+      }
+      if (action == 'toggle') {
+        action = !group.on
+        console.log(`toggling from ${group.on} to ${action}`);
+      }
+      if (action !== undefined) {
+        console.log(`switching from ${group.on} to ${action}`);
+        group.on = action;
+        return client.groups.save(group).then( function (x) {
+          console.log("saved");
+        });
       }
 
-      console.log(`  Actions:`);
-      for (let action of rule.actions) {
-        console.log(`    Address: ${action.address}`);
-        console.log(`    Method:  ${action.method}`);
-        console.log(`    Body:    ${JSON.stringify(action.body)}`);
-        console.log();
+      console.log(`Group [${group.id}]: ${group.name}`);
+      console.log(`  Type: ${group.type}`);
+      console.log(`  Class: ${group.class}`);
+      console.log('  Light Ids: ' + group.lightIds.join(', '));
+      console.log('  State:');
+      console.log(`    Any on:     ${group.anyOn}`);
+      console.log(`    All on:     ${group.allOn}`);
+      console.log('  Action:');
+      console.log(`    On:         ${group.on}`);
+      console.log(`    Brightness: ${group.brightness}`);
+      console.log(`    Color mode: ${group.colorMode}`);
+      console.log(`    Hue:        ${group.hue}`);
+      console.log(`    Saturation: ${group.saturation}`);
+//      console.log(`    X/Y:        ${group.xy[0]}, ${group.xy[1]}`);
+      console.log(`    Color Temp: ${group.colorTemp}`);
+      console.log(`    Alert:      ${group.alert}`);
+      console.log(`    Effect:     ${group.effect}`);
+
+      if (group.modelId !== undefined) {
+        console.log(`  Model Id: ${group.modelId}`);
+        console.log(`  Unique Id: ${group.uniqueId}`);
+        console.log('  Model:');
+        console.log(`    Id:           ${group.model.id}`);
+        console.log(`    Manufacturer: ${group.model.manufacturer}`);
+        console.log(`    Name:         ${group.model.name}`);
+        console.log(`    Type:         ${group.model.type}`);
       }
 
       console.log();
@@ -148,22 +221,3 @@ promise.then( function (ip) {
   });
 })
 
-
-
-.then( function (x) {
-	return client.groups.getById(0)
-  .then(group => {
-    console.log('Special group 0');
-    console.log(JSON.stringify(group,0,4));
-    console.log('  Light Ids:', group.lightIds.join(', '));
-
- 	if (action !== undefined) {
-	    group.on = action;
-	    console.log(JSON.stringify(group,0,4));
-
-	    return client.groups.save(group).then( function (x) {
-			console.log("saved");
-		});
-	}
-  });
-})
